c1c0ca04078d89cf0444a7db40e3f3be91f269c5
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-04-11 16:06:56 +0900

    IGNORE ---

--- src/main/java/com/jhsung/config/security/WebSecurityConfig.java
+++ src/main/java/com/jhsung/config/security/WebSecurityConfig.java
@@ -9,25 +9,26 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
 			// 비인증 접근 허용 URL 설정
 			// 신규 일반 페이지(/, /login, /logout) 생성 시 추가를 안해주면, 비인증 시 Login 페이지로 Redirect.
             // 신규 보안 페이지 생성 시 추가를 깜빡하더라도 보안상으로는 더 좋음.
 		    // 위에서 설정한 URL 외의 모든 요청은 미인증 시 접근 불가
 			.authorizeRequests()
-			.antMatchers("/").permitAll()
-			.antMatchers("/login").permitAll()
-			.antMatchers("/logout").permitAll()
+			.antMatchers(
+					"/",
+					"/login",
+					"/logout").permitAll()
 			.anyRequest().authenticated();
 		
-		http
+		httpSecurity
 			// 보안 페이지 요청 시, Login 페이지로 Redirect
 			// 컨트롤러 매핑을 하지 않으면 기본 제공되는 로그인 페이지가 뜬다.
 			.formLogin()
 			.loginProcessingUrl("/login");
 		
-		http
+		httpSecurity
 			.logout()
 			// /logout 을 호출할 경우 로그아웃
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
