95bb1921dc38448c2b64710a0a69ad046e91c7ce
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-05-02 13:25:25 +0900

    HandlerInterceptorAdapter 구현 & 적용, logging.level 변경

---
+++ src/main/java/com/jhsung/common/config/interceptor/GlobalHttpInterceptor.java
@@ -0,0 +1,33 @@
+package com.jhsung.common.config.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class GlobalHttpInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.debug("preHandle in HandlerInterceptor ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		log.debug("request.getRequestURL() : {}", request.getRequestURL().toString());
+		log.debug("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		log.debug("postHandle in HandlerInterceptor ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ-");
+		log.debug("response.getContentType : {}", response.getContentType());
+		log.debug("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+	}
+
+}
---
+++ src/main/java/com/jhsung/common/config/mvc/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.jhsung.common.config.mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.jhsung.common.config.interceptor.GlobalHttpInterceptor;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private GlobalHttpInterceptor globalHttpInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(globalHttpInterceptor)
+				.addPathPatterns("/**");
+	}
+
+}
