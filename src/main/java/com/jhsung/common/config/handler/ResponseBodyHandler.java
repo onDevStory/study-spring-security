b3e5bbf26a2e2766446b70246ee60ca5c230c253
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-05-11 13:36:48 +0900

    implements ResponseBodyAdvice

---
+++ src/main/java/com/jhsung/common/config/handler/ResponseBodyHandler.java
@@ -0,0 +1,34 @@
+package com.jhsung.common.config.handler;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import com.jhsung.common.ResponseFormat;
+
+@ControllerAdvice
+public class ResponseBodyHandler implements ResponseBodyAdvice<Object> {
+
+	@Override
+	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+		// return ResponseFormat if there is exception occurence. no-execute beforeBodyWrite
+		if (returnType.getParameterType() == ResponseFormat.class) {
+			return false;
+		}
+		// return Object if no-exception. execute beforeBodyWrite 
+		return true;
+	}
+
+	@Override
+	public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+			Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+			ServerHttpResponse response) {
+		System.out.println("~~~ResponseBodyHandler beforeBodyWrite");
+		return new ResponseFormat(body);
+	}
+
+}
